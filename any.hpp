c92483bfb79f403b0c2860436ae2ff6c92db6bf2
Mykhailo Maidan <mykhailo.maidan@edvantis.com>
2021-05-05 21:53:29 +0300

    Add any_cast

--- any.hpp
+++ any.hpp
@@ -10,7 +10,7 @@ namespace mtl
         template<class U>
         any(U val) 
         {
-            m_AnyImp = new any_storage<U>();
+            m_AnyImp = new any_storage<U>(val);
         }
         private:
         class any_base 
@@ -20,13 +20,28 @@ namespace mtl
         template<class T>
         class any_storage : public any_base
         {
-            T* data{nullptr};
+            
             public:
-            any_storage(T)
+            any_storage(T val) : m_Data(val) 
+            {}
+            T m_Data;
         };
 
         any_base*  m_AnyImp{nullptr};
-        
 
+        template<class F>
+        friend F any_cast(any& obj);
     };
+
+    template<class F>
+    F any_cast(any& obj)
+    {
+        auto any_obj = static_cast<any::any_storage<F>*>(obj.m_AnyImp);
+        if(any_obj != nullptr)
+        {
+            return any_obj->m_Data;
+        }
+    }
+
+
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "any.hpp"
 int main()
 {
     std::cout << "hello world" << std::endl;
+    mtl::any myAny = std::string("Hello value");
+    
+    std::cout << "Value:" << mtl::any_cast<std::string>(myAny);
     return 0;
 }
